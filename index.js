61f7c9b1550cd0f7e5d3129ef808d7fdad081a51
Rodrigo914 <rodrigovazquez914@gmail.com>
2020-03-28 13:28:36 -0600

    Matriz determinantes cofactores

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const getTriangleArea = require('./functions/triangle-area');
 const getCircleArea = require('./functions/circle-area');
 const squareOperations = require('./functions/square-operations');
+const Matrix = require('./functions/Matrix');
 
 
 console.log('Test');
@@ -10,3 +11,16 @@ console.log('El area de mi circulo es: ', getCircleArea(5));
 console.log('el area del cuadrado es: ', squareOperations.getArea(8));
 console.log('el area del cuadrado es: ', squareOperations.getPerimeter(8));
 console.log('el area del cuadrado es: ', squareOperations.getDeagonal(8));
+
+const MatrixT = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+];
+
+const myMatrixT= Matrix(MatrixT);
+
+console.log(myMatrixT.getDeterminante());
+
+
